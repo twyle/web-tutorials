f4374107d946e38196c93f99e8a4221e040be5e6
twyle <lyceokoth@gmail.com>
2023-03-07 14:08:01 +0300

    creates the main section

--- youtube-clone/js/index.js
+++ youtube-clone/js/index.js
@@ -1,7 +1,9 @@
 const menuIcon = document.querySelector(".menu-icon");
 const sideBar = document.querySelector(".sidebar");
+const container = document.querySelector(".container");
 
 
 menuIcon.onclick = () => {
     sideBar.classList.toggle('small-sidebar')
+    container.classList.toggle('large-container')
 }
